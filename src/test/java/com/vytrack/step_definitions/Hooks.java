e98545af21044a67bf00a1e8594a8d1b805b8381
alireza8260 <alireza8260@gmail.com>
2025-02-27 16:35:45 -0800

    update pom File

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -24,9 +24,10 @@ public class Hooks {
     @Before
     public void setupMethod(){
 
-        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
 
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
     //@Before (value = "@login", order = 2 )
